d1e37cb7b5ea9543387c7a271044d8ccf29cd81d
cypherm0nk <88748637+cypherm0nk@users.noreply.github.com>
2025-04-28 07:23:52 -0300

    creating the sign up form

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+  loginForm=this.formbuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+  signUpForm=this.formbuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password:['',[Validators.required,Validators.minLength(6)]]
+  })
+  constructor(private readonly formbuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.loginCard = true;
+  }
+  onSubmitLoginForm():void {
+    console.log("dados do form de login",this.loginForm.value);
+  }
+  onSubmitLoginSignUpFormForm():void {
+    console.log("dados do form de sign up",this.signUpForm.value);
+  }
+  login() {
+    if (this.loginForm.valid) {
+      // Perform login action
+      console.log('Login successful');
+    } else {
+      // Handle form validation errors
+      console.log('Form is invalid');
+    }
+  }
+
 }
